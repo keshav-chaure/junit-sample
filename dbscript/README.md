53761af981a960c66fab3315ea77856f874fcb3a
keshav-chaure <keshav.chaure@gmail.com>
2018-01-07 15:08:29 +0530

    db script readme updated.

--- dbscript/README.md
+++ dbscript/README.md
@@ -1,4 +1,3 @@
 ### Database script for mysql
 # use this script sequentially.
 ##### 1. creatingDB
-##### 2. creatingTableEmployees
--- dbscript/cratingDB.sql
+++ dbscript/cratingDB.sql
@@ -1,8 +1,12 @@
+-- create database mydb
 CREATE DATABASE mydb;
 USE mydb;
--- CREATE TABLE EMPLOYEE (name VARCHAR(20), id VARCHAR(20));
+-- create table  EMPLOYEE;
 CREATE  TABLE `mydb`.`EMPLOYEE` (
   `name` VARCHAR(20) NOT NULL ,
   `id` INT NOT NULL ,
   PRIMARY KEY (`id`) );
+-- inserted two records
 INSERT INTO `mydb`.`employee` (`name`, `id`) VALUES ('kc', 2);
+INSERT INTO `mydb`.`employee` (`name`, `id`) VALUES ('ram',3);
+--
